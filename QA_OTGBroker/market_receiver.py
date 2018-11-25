6fe9902027458dadc83f2770fc2b15f4010b1893
yutiansut <yutiansut@qq.com>
2018-11-25 16:39:40 +0800

    #

---
+++ QA_OTGBroker/market_receiver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        
+        print(message)
+        await websocket.send(message)
+
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 7890))
+asyncio.get_event_loop().run_forever()
+
