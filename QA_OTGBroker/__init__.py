4193782c58622cad7e6d8480254ae094d3a34252
yutiansut <yutiansut@qq.com>
2019-03-09 04:25:51 +0800

    #update

--- QA_OTGBroker/__init__.py
+++ QA_OTGBroker/__init__.py
@@ -7,94 +7,6 @@ except ImportError:
     import _thread as thread
 import time
 
-
-def send_order(account_cookie, order_direction='BUY', order_offset='OPEN', volume=1, order_id=False, code='rb1905', exchange_id='SHFE', price=3925):
-    """[summary]
-
-    Arguments:
-        account_cookie {[type]} -- [description]
-
-    Keyword Arguments:
-        order_direction {str} -- [description] (default: {'BUY'})
-        order_offset {str} -- [description] (default: {'OPEN'})
-        volume {int} -- [description] (default: {1})
-        order_id {bool} -- [description] (default: {False})
-        code {str} -- [description] (default: {'rb1905'})
-        exchange_id {str} -- [description] (default: {'SHFE'})
-
-    Returns:
-        [type] -- [description]
-    """
-
-    return json.dumps({
-        "aid": "insert_order",                  # //必填, 下单请求
-        # //必填, 需要与登录用户名一致, 或为登录用户的子账户(例如登录用户为user1, 则报单 user_id 应当为 user1 或 user1.some_unit)
-        "user_id": account_cookie,
-        # //必填, 委托单号, 需确保在一个账号中不重复, 限长512字节
-        "order_id": order_id if order_id else QA.QA_util_random_with_topic('QAOTG'),
-        "exchange_id": exchange_id,  # //必填, 下单到哪个交易所
-        "instrument_id": code,               # //必填, 下单合约代码
-        "direction": order_direction,                      # //必填, 下单买卖方向
-        # //必填, 下单开平方向, 仅当指令相关对象不支持开平机制(例如股票)时可不填写此字段
-        "offset":  order_offset,
-        "volume":  volume,                             # //必填, 下单手数
-        "price_type": "LIMIT",  # //必填, 报单价格类型
-        "limit_price": price,  # //当 price_type == LIMIT 时需要填写此字段, 报单价格
-        "volume_condition": "ANY",
-        "time_condition": "GFD",
-    })
-
-
-def cancel_order(account_cookie, order_id):
-    return json.dumps({
-        "aid": "cancel_order",  # //必填, 撤单请求
-        "user_id": account_cookie,  # //必填, 下单时的 user_id
-        "order_id": order_id               # //必填, 委托单的 order_id
-    })
-
-
-def transfer(account_cookie, password, bankid, bankpassword, amount):
-    return json.dumps({
-        {
-            "aid": "req_transfer",  # //必填, 转账请求
-            "future_account": account_cookie,  # //必填, 期货账户
-            "future_password": password,  # //必填, 期货账户密码
-            "bank_id": bankid,  # //必填, 银行ID
-            "bank_password": bankpassword,  # //必填, 银行账户密码
-            "currency": "CNY",  # //必填, 币种代码
-            "amount": float(amount)  # //必填, 转账金额, >0 表示转入期货账户, <0 表示转出期货账户
-        }
-    })
-
-
-def subscribe_quote(ins_list="SHFE.cu1612,CFFEX.IF1701"):
-    return json.loads(
-        {
-            "aid": "subscribe_quote",  # // 必填, 请求订阅实时报价数据
-            "ins_list": ins_list  # // 必填, 需要订阅的合约列表，以逗号分隔
-        })
-
-
-def peek():
-    return json.dumps(
-        {
-            "aid": "peek_message"
-        })
-
-
-def login(name='131176', password='qchl1234', broker='simnow'):
-    return json.dumps({
-        "aid": "req_login",
-        "bid": str(broker),
-        "user_name": str(name),
-        "password": str(password),
-    })
-
-
-def ping(ws):
-    return ws.ping()
-
-
 """3种
 
 
@@ -366,6 +278,93 @@ notify
 """
 
 
+def send_order(account_cookie, order_direction='BUY', order_offset='OPEN', volume=1, order_id=False, code='rb1905', exchange_id='SHFE', price=3925):
+    """[summary]
+
+    Arguments:
+        account_cookie {[type]} -- [description]
+
+    Keyword Arguments:
+        order_direction {str} -- [description] (default: {'BUY'})
+        order_offset {str} -- [description] (default: {'OPEN'})
+        volume {int} -- [description] (default: {1})
+        order_id {bool} -- [description] (default: {False})
+        code {str} -- [description] (default: {'rb1905'})
+        exchange_id {str} -- [description] (default: {'SHFE'})
+
+    Returns:
+        [type] -- [description]
+    """
+
+    return json.dumps({
+        "aid": "insert_order",                  # //必填, 下单请求
+        # //必填, 需要与登录用户名一致, 或为登录用户的子账户(例如登录用户为user1, 则报单 user_id 应当为 user1 或 user1.some_unit)
+        "user_id": account_cookie,
+        # //必填, 委托单号, 需确保在一个账号中不重复, 限长512字节
+        "order_id": order_id if order_id else QA.QA_util_random_with_topic('QAOTG'),
+        "exchange_id": exchange_id,  # //必填, 下单到哪个交易所
+        "instrument_id": code,               # //必填, 下单合约代码
+        "direction": order_direction,                      # //必填, 下单买卖方向
+        # //必填, 下单开平方向, 仅当指令相关对象不支持开平机制(例如股票)时可不填写此字段
+        "offset":  order_offset,
+        "volume":  volume,                             # //必填, 下单手数
+        "price_type": "LIMIT",  # //必填, 报单价格类型
+        "limit_price": price,  # //当 price_type == LIMIT 时需要填写此字段, 报单价格
+        "volume_condition": "ANY",
+        "time_condition": "GFD",
+    })
+
+
+def cancel_order(account_cookie, order_id):
+    return json.dumps({
+        "aid": "cancel_order",  # //必填, 撤单请求
+        "user_id": account_cookie,  # //必填, 下单时的 user_id
+        "order_id": order_id               # //必填, 委托单的 order_id
+    })
+
+
+def transfer(account_cookie, password, bankid, bankpassword, amount):
+    return json.dumps({
+        {
+            "aid": "req_transfer",  # //必填, 转账请求
+            "future_account": account_cookie,  # //必填, 期货账户
+            "future_password": password,  # //必填, 期货账户密码
+            "bank_id": bankid,  # //必填, 银行ID
+            "bank_password": bankpassword,  # //必填, 银行账户密码
+            "currency": "CNY",  # //必填, 币种代码
+            "amount": float(amount)  # //必填, 转账金额, >0 表示转入期货账户, <0 表示转出期货账户
+        }
+    })
+
+
+def subscribe_quote(ins_list="SHFE.cu1612,CFFEX.IF1701"):
+    return json.loads(
+        {
+            "aid": "subscribe_quote",  # // 必填, 请求订阅实时报价数据
+            "ins_list": ins_list  # // 必填, 需要订阅的合约列表，以逗号分隔
+        })
+
+
+def peek():
+    return json.dumps(
+        {
+            "aid": "peek_message"
+        })
+
+
+def login(name='131176', password='qchl1234', broker='simnow'):
+    return json.dumps({
+        "aid": "req_login",
+        "bid": str(broker),
+        "user_name": str(name),
+        "password": str(password),
+    })
+
+
+def ping(ws):
+    return ws.ping()
+
+
 def parse_rtn(message):
     pass
 
@@ -417,23 +416,8 @@ def on_open(ws):
         ws.send(peek())
         for i in range(100):
             ws.sock.ping('QUANTAXIS')
-            # ws.sock.pong('hi')
             time.sleep(1)
-        #ws.send(send_order(acc1, 'BUY'))
         time.sleep(20)
         ws.close()
         print("thread terminating...")
     thread.start_new_thread(run, ())
-
-
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://www.yutiansut.com:7988",
-                                on_pong=on_pong,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.on_ping = on_ping
-
-    ws.run_forever(ping_interval=0)
