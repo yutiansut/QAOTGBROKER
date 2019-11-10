e68f68f4a2698b1215f23256cd1cd189dc3c2757
yutiansut <yutiansut@qq.com>
2019-11-11 03:23:16 +0800

    1.9.2

--- setup.py
+++ setup.py
@@ -11,6 +11,34 @@ try:
 except:
     from distutils.core import setup
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel, get_platform
+
+    class bdist_wheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package (we have platform specific ctpse lib)
+            # if self.plat_name != "any":
+            #     self.root_is_pure = False
+            #     plat_name = (self.plat_name or get_platform()).replace('-', '_').replace('.', '_')
+            #     if plat_name == "linux_x86_64" or plat_name == "manylinux1_x86_64":
+            #         self.distribution.package_data[""] = ["ctpse/LinuxDataCollect64.so"]
+            #     elif plat_name == "win32":
+            #         self.distribution.package_data[""] = ["ctpse/WinDataCollect32.dll"]
+            #     elif plat_name == "win_amd64":
+            #         self.distribution.package_data[""] = ["ctpse/WinDataCollect64.dll"]
+            self.distribution.package_data[""] = ["ctpse/WinDataCollect64.dll","ctpse/LinuxDataCollect64.so","ctpse/WinDataCollect32.dll"]
+        def get_tag(self):
+            # this set's us up to build generic wheels.
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            
+            plat =  "any"
+            python, abi = 'py3', 'none'
+            return python, abi, plat
+except ImportError:
+    bdist_wheel = None
 
 NAME = "quantaxis_otgbroker"
 """
@@ -32,7 +60,7 @@ LICENSE = "MIT"
 
 setup(
     name=NAME,
-    version='1.9',
+    version='1.9.2',
     description=DESCRIPTION,
     long_description='publisher and subscriber',
     classifiers=[
@@ -47,6 +75,7 @@ setup(
             'QAOTG_test=QA_OTGBroker.app:app'
         ]
     },
+    cmdclass={'bdist_wheel': bdist_wheel},
     keywords=KEYWORDS,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
